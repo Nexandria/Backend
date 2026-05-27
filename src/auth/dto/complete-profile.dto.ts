4fb2b5ee0b0df32c26ce03d55b2f2c620421ea44
LautaroGarc <lautaro.garcia.et21.21@gmail.com>
2026-05-27 09:17:22 -0300

    prisma module

--- src/auth/dto/complete-profile.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Gender } from '../../../generated/prisma';
-
-export class CompleteProfileDto {
-  fullName!: string;
-  dni!: number;
-  address!: string;
-  birth!: string;
-  gender!: Gender;
-}

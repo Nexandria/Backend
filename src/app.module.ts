0bf1227bce932a10e5932aab37563e0ba384a0d0
LautaroGarc <lautaro.garcia.et21.21@gmail.com>
2026-09-03 09:22:54 -0300

    Prisma & Redis module + dockerfiles

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    RedisModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
